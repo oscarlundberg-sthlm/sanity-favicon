2727732ff893635c56adc9df855da0bd51fe8421
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2023-08-03 11:32:10 +0200

    wip

--- src/convertAndMove.js
+++ src/convertAndMove.js
@@ -1,9 +1,16 @@
+import { dotenv } from "dotenv";
 import { exec } from "node:child_process";
+dotenv.config();
 
 export default (
     inputImagePath, 
     { targetFolder }
 ) => {
+    const target = 
+        process.env.NODE_ENV === 'production' ? 
+        '/favicon.ico' : 
+        `${targetFolder}/favicon.ico`;
+
     return new Promise((resolve, reject) => {
         
         const childProcessCallback = (error, stdout, stderr) => {
@@ -18,12 +25,12 @@ export default (
 
         if (inputImagePath.endsWith('.ico')) {
             exec(
-                `mv ${inputImagePath} ${targetFolder}/favicon.ico`, 
+                `mv ${inputImagePath} ${target}`, 
                 childProcessCallback
             );
         } else {
             exec(
-                `magick convert ${inputImagePath} -background transparent -define icon:auto-resize=16,24,32,48,64,72,96,128,256 ${targetFolder}/favicon.ico && rm ${inputImagePath}`, 
+                `magick convert ${inputImagePath} -background transparent -define icon:auto-resize=16,24,32,48,64,72,96,128,256 ${target} && rm ${inputImagePath}`, 
                 childProcessCallback
             );
         }
